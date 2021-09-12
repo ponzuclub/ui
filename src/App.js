f6e999ba86578f515c48db2542a1e972ecfaa476
0xpointer <0xpointer@protonmail.com>
2021-09-12 13:35:04 -0400

    Update index, add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import { Navbar, Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>PonzuClub</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            Connect Wallet
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
