0ba24865ba99216bcf2c0545d796348f6bac5353
0xpointer <0xpointer@protonmail.com>
2021-09-12 15:50:22 -0400

    Add farming cards

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import logo from './logo512.png';
-import { Button, Navbar, Container, Row, Col, Image } from 'react-bootstrap';
+import { Button, Card, Navbar, Container, Row, Col, Image, FormControl, InputGroup } from 'react-bootstrap';
 import { useWeb3React } from '@web3-react/core';
 import { injected } from './connectors';
 import { addArbitrumRPC } from './hooks';
@@ -23,7 +23,7 @@ function Home() {
     <div className="Home">
         <Navbar>
         <Container>
-            <Navbar.Brand>PonzuClub</Navbar.Brand>
+            <Navbar.Brand>ponzu.club</Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
             {chainId != 42161 ? <Button variant="secondary" onClick={handleSwitchNetwork(library)}>Switch to Arbitrum</Button> : null}
@@ -37,10 +37,85 @@ function Home() {
             <Image src={logo} rounded />
             </Col>
             <Col>
-                Welcome to Ponzu Club
+                Welcome to ponzu.club
             </Col>
+        </Row>
+        <Row>
             <Col>
-                
+                <Card>
+                    <Card.Header as="h5">Farm</Card.Header>
+                    <Card.Body>
+                        <Card.Title>ETH Pool</Card.Title>
+                        <Card.Text>
+                            APR:
+                        </Card.Text>
+                        <Row>
+                            <Col>
+                                <Button variant="primary">Stake</Button>
+                            </Col>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                                </InputGroup>
+                            </Col>
+                            <Col>
+                                <Button variant="success">Max</Button>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <Card.Header as="h5">Farm</Card.Header>
+                    <Card.Body>
+                        <Card.Title>NYAN-PONZU LP Pool</Card.Title>
+                        <Card.Text>
+                            APR:
+                        </Card.Text>
+                        <Row>
+                            <Col>
+                                <Button variant="primary">Add</Button>
+                            </Col>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                                </InputGroup>
+                            </Col>
+                            <Col>
+                                <Button variant="success">Max</Button>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <Card.Header as="h5">Farm</Card.Header>
+                    <Card.Body>
+                        <Card.Title>PONZU LP Pool</Card.Title>
+                        <Card.Text>
+                            APR:
+                        </Card.Text>
+                        <Row>
+                            <Col>
+                                <Button variant="primary">Stake</Button>
+                            </Col>
+                            <Col>
+                                <InputGroup className="mb-3">
+                                    <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                                </InputGroup>
+                            </Col>
+                            <Col>
+                                <Button variant="success">Max</Button>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
             </Col>
         </Row>
         </Container>
